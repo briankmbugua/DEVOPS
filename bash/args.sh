082a189554c6f440ba6bba8dd2fde4228105f386
briankmbugua <briankmbuguak@gmail.com>
2023-08-13 00:00:02 +0300

    all the files

---
+++ bash/args.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# command substitution
+lsResult=$(ls)
+echo "My files are:"$lsResult
--- bash/variabledecla.sh
+++ bash/variabledecla.sh
@@ -12,6 +12,31 @@ echo 'But not single-quoted strings. This will not read the varible: $MY_VARIABL
 6. No need of using any quotes either single or double to define a variable with single character value.To input words or strings use qoutes for enclosing your content in that variable
 7. Bash variables are untyped
 TYPES OF BASH VARIABLES
-1. System-Defined Variables
+1. System-Defined Variables - this are predefined variables and are maintained by the linux system itself.They are generally defined in capital letters
 2.User-Defined Variables
 COMMENT
+#BASH
+echo $BASH
+#BASH_VERSION
+echo $BASH_VERSION
+#COLUMNS specify the bumber of columns for our screen
+echo $COLUMNS
+#HOME specify the home directory for the user
+echo $HOME
+#LOGNAME login user name
+echo $LOGNAME
+#OSTYPE
+echo $OSTYPE
+#PWD
+#USERNAME specify the name of the currently logged in user
+#To know the list of variables in the system use set, env and printenv
+
+#USER DEFINED VARIABLES created and maintained by the user
+name=peter
+ROLL_NO=52345
+echo "The student name is $name and his roll number is $ROLL_NO"
+#Combining two string variables
+Bash_var1="brian"
+Bash_var2="mbugua"
+echo "$Bash_var1$Bash_var2"
+echo "My name is $Bash_var1$Bash_var2"
